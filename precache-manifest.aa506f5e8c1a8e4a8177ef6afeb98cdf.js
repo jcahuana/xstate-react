281adf2346cdfc239bed0b98ee9313d76f073572
Juan Cahuana <jcahuanam@gmail.com>
2020-05-31 17:57:39 -0500

    Updates

---
+++ precache-manifest.aa506f5e8c1a8e4a8177ef6afeb98cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71bd58e8c97cf35a82dabbda2cdb046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e628c7cda0eefaafb1",
+    "url": "/static/css/main.3325237d.chunk.css"
+  },
+  {
+    "revision": "33d11770e5eec3efd88d",
+    "url": "/static/js/2.1c688af0.chunk.js"
+  },
+  {
+    "revision": "5839521b381d6ae8d4739a6544c25d5a",
+    "url": "/static/js/2.1c688af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e628c7cda0eefaafb1",
+    "url": "/static/js/main.97132a8a.chunk.js"
+  },
+  {
+    "revision": "ef75a6ed8a6174a01bc6",
+    "url": "/static/js/runtime-main.225e90d1.js"
+  }
+]);
